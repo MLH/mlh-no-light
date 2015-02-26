291c3630e5db39fa36b4eb83c2e9ae1c8b2cb71a
Nick Quinlan <nick@nicholasquinlan.com>
2015-02-26 10:19:50 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+	$("#code").focus();
+
+	$("#code").keydown(function (e) {
+		if(e.keyCode === 9) {
+			e.preventDefault();
+		}
+	});
+
+	$("#logo").dblclick(function (e) {
+		e.preventDefault();
+		$("#code").focus();
+
+		var c = confirm("Are you sure you want to render your code?\n\nYou will not be able to come back to it.");
+		if(c) {
+			$("html").html($("#code").val());
+		}
+
+	});
+
+	window.addEventListener("beforeunload", function (e) {
+		var confirmationMessage = "Are you sure you want to leave the page? All your code will be lost!";
+
+		(e || window.event).returnValue = confirmationMessage;     //Gecko + IE
+		return confirmationMessage;                                //Gecko + Webkit, Safari, Chrome etc.
+	});
+});
