6ad0010e020b4e8a9c9c3de281a98c3ffc02e346
Bilawal Hameed <bilawal@studenthack.com>
2015-10-17 17:18:05 +0100

    Disabled Ruby 'find' and initializers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,11 +16,11 @@ require 'rack/test'
 require 'faker'
 require 'database_cleaner'
 
-require 'find'
+# require 'find'
 
-%w{./config/initializers ./lib}.each do |load_path|
-  Find.find(load_path) { |f| require f if f.match(/\.rb$/) }
-end
+# %w{./config/initializers ./lib}.each do |load_path|
+#   Find.find(load_path) { |f| require f if f.match(/\.rb$/) }
+# end
 
 class MiniTest::Spec
   include Rack::Test::Methods
