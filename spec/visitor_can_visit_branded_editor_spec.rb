486bf4a32a65f9fbc7f0a1e807e77c1e975e6717
Swift <theycallmeswift@gmail.com>
2017-05-31 16:28:53 -0400

    Removed branding from default page, fixed tests (#11)

--- spec/visitor_can_visit_branded_editor_spec.rb
+++ spec/visitor_can_visit_branded_editor_spec.rb
@@ -8,6 +8,14 @@ end
 describe NoLightSinatra do
 
   it 'loads the default editor' do
+    visit_branded_editor(nil)
+
+    page.find_classes('body.editor').wont_include       :dell
+    page.find('#submission_html')["placeholder"].must_equal 'Enter your code here ...'
+    page.find('#submit').text.must_equal                    '!light'
+  end
+
+  it 'loads the dell branded editor' do
     visit_branded_editor(:dell)
 
     page.find_classes('body.editor').must_include           :dell
